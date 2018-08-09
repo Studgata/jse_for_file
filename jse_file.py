074091b4074708e1d01c2ca8b4c6fc42a50f81a6
Studgata <34764380+Studgata@users.noreply.github.com>
2018-08-09 12:01:47 +0530

    Update jse_file.py

--- jse_file.py
+++ jse_file.py
@@ -1,14 +1,9 @@
-import socket
-s = socket.socket()
-host = ""
-port = 1001
-addr = (host,port)
-s.bind(addr)
+import itertools
 
 
 def division():
     # number of lines
-    seg = 10
+    seg = 100
     num_line = 0
     for x in open("testfle.txt"):
         num_line += 1
@@ -22,93 +17,28 @@ def division():
     if ext != 0:
         chunks += 1
     # division
-
-    print(chunks)
-
-
-    holder = {}
-    h = []
-    k = 0
-    p = 0
-    chunk = 0
-    count = 1
     fil = open('testfle.txt')
     filread = fil.readlines()
-    while k < len(filread)-1:
-        while p < seg:
-            h.append(filread[k])
-            k = k +1
-            p= p+1
-            holder.update({chunk:h})
-        h = []
-        chunk = chunk +1
-        p = 0
+    print(chunks)
+    holder = {}
+    # h = []
+    k = 0
+    chunk = 0
+    for i in range(chunks):
+        k = k + 1
 
-
-    for x,y in holder.items():   
-        print(x,y)
-    print(len(holder))
-            
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-    while (count < (chunks + 1)):
-        for line in filread:
-            e = len(lst)
-
-            if (e != seg):
-                lst.append(line)
-            else:
-                chk = chk + seg
-                count = count + 1
-                e = -1
-                break
-            holder.update({count: lst})
-            
-            lst = []
-    return holder
-        
-"""
-
+        holder.update({chunk: filread[i * 10:(i + 1) * 10]})
+        chunk = chunk + 1
+    for x, y in holder.items():
+        print(x, y)
 
 
 def jse():
-    fle = division()
-        
-    #for key, value in fle.items() :
-     #   print (key, value)
-    print(fle)    
+    division()
+
+
 def main():
     jse()
-
-
-
 
 
 if __name__ == "__main__":
